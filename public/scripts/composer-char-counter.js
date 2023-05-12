22427e69efb0b9f044cec94054b1389f96234e03
Nanaya Miki <nanayamiki@gmail.com>
2023-05-12 15:05:14 +0000

    implement the char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	// --- our code goes here ---
+	const $textarea = $(".new-tweet-textarea");
+
+	$textarea.on("input", function (e) {
+		e.preventDefault;
+		const $tweetLength = $(this).val().length;
+		const $counter = $(this).next().children(".counter");
+		$counter.text(140 - $tweetLength);
+		if ($tweetLength > 140) {
+			$counter.addClass("invalid");
+		} else {
+			$counter.removeClass("invalid");
+		}
+	});
+});
